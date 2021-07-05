3cce1dc66812960664047d8daf33db0f9a976d82
Johannes <johannes-ruf@gmx.de>
2021-07-05 13:00:11 +0200

    test

--- dreamer-pytorch/main.py
+++ dreamer-pytorch/main.py
@@ -2,42 +2,37 @@ import datetime
 import os
 import argparse
 import torch
+from rlpyt.samplers.collections import TrajInfo
 
 from rlpyt.runners.minibatch_rl import MinibatchRlEval, MinibatchRl
 from rlpyt.samplers.serial.sampler import SerialSampler
 from rlpyt.utils.logging.context import logger_context
 
-from dreamer.agents.atari_dreamer_agent import AtariDreamerAgent
+from dreamer.agents.dmc_dreamer_agent import DMCDreamerAgent
 from dreamer.algos.dreamer_algo import Dreamer
-from dreamer.envs.atari import AtariEnv, AtariTrajInfo
 from dreamer.envs.wrapper import make_wapper
-from dreamer.envs.one_hot import OneHotAction
+from dreamer.envs.dmc import DeepMindControl
+from dreamer.envs.action_repeat import ActionRepeat
+from dreamer.envs.normalize_actions import NormalizeActions
+
 from dreamer.envs.time_limit import TimeLimit
 
 
-def build_and_train(log_dir, game="pong", run_ID=0, cuda_idx=None, eval=False, save_model='last', load_model_path=None):
+def build_and_train(log_dir, game="walker", run_ID=0, cuda_idx=None, eval=False, save_model='last', load_model_path=None):
     params = torch.load(load_model_path) if load_model_path else {}
     agent_state_dict = params.get('agent_state_dict')
     optimizer_state_dict = params.get('optimizer_state_dict')
 
     action_repeat = 2
-    env_kwargs = dict(
-        name=game,
-        action_repeat=action_repeat,
-        size=(64, 64),
-        grayscale=False,
-        life_done=True,
-        sticky_actions=True,
-    )
     factory_method = make_wapper(
-        AtariEnv,
-        [OneHotAction, TimeLimit],
-        [dict(), dict(duration=1000 / action_repeat)])
+        DeepMindControl,
+        [ActionRepeat, NormalizeActions, TimeLimit],
+        [dict(amount=action_repeat), dict(), dict(duration=1000 / action_repeat)])
     sampler = SerialSampler(
         EnvCls=factory_method,
-        TrajInfoCls=AtariTrajInfo,  # default traj info + GameScore
-        env_kwargs=env_kwargs,
-        eval_env_kwargs=env_kwargs,
+        TrajInfoCls=TrajInfo,
+        env_kwargs=dict(name=game),
+        eval_env_kwargs=dict(name=game),
         batch_T=1,
         batch_B=1,
         max_decorrelation_steps=0,
@@ -45,16 +40,15 @@ def build_and_train(log_dir, game="pong", run_ID=0, cuda_idx=None, eval=False, s
         eval_max_steps=int(10e3),
         eval_max_trajectories=5,
     )
-    algo = Dreamer(horizon=10, kl_scale=0.1, use_pcont=True, initial_optim_state_dict=optimizer_state_dict)
-    agent = AtariDreamerAgent(train_noise=0.4, eval_noise=0, expl_type="epsilon_greedy",
-                              expl_min=0.1, expl_decay=2000 / 0.3, initial_model_state_dict=agent_state_dict,
-                              model_kwargs=dict(use_pcont=True))
+    algo = Dreamer(initial_optim_state_dict=optimizer_state_dict)
+    agent = DMCDreamerAgent(train_noise=0.3, eval_noise=0, expl_type="additive_gaussian",
+                            expl_min=None, expl_decay=None, initial_model_state_dict=agent_state_dict)
     runner_cls = MinibatchRlEval if eval else MinibatchRl
     runner = runner_cls(
         algo=algo,
         agent=agent,
         sampler=sampler,
-        n_steps=5e3,
+        n_steps=5e6,
         log_interval_steps=1e3,
         affinity=dict(cuda_idx=cuda_idx),
     )
@@ -67,9 +61,9 @@ def build_and_train(log_dir, game="pong", run_ID=0, cuda_idx=None, eval=False, s
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--game', help='Atari game', default='pong')
+    parser.add_argument('--game', help='Atari game', default='cheetah_run')
     parser.add_argument('--run-ID', help='run identifier (logging)', type=int, default=0)
-    parser.add_argument('--cuda-idx', help='gpu to use ', type=int, default=None)
+    parser.add_argument('--cuda-idx', help='gpu to use ', type=int, default=0)
     parser.add_argument('--eval', action='store_true')
     parser.add_argument('--save-model', help='save model', type=str, default='last',
                         choices=['all', 'none', 'gap', 'last'])
