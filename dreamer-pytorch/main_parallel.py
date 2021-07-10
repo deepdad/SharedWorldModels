bfa90532f8e60e697b7ba28a2dcae24c4463d3ca
deepdad <templepack@protonmail.com>
2021-07-10 20:32:39 +0200

    initial code for CPU parallellization

--- dreamer-pytorch/main_parallel.py
+++ dreamer-pytorch/main_parallel.py
@@ -20,6 +20,7 @@ from dreamer.envs.normalize_actions import NormalizeActions
 
 from dreamer.envs.time_limit import TimeLimit
 
+# N means not in dreamer.experiments.scripts.train -> maybe remove these below
 
 def build_and_train(log_dir, task="TargetReach", environments=RLBench, run_ID=0, cuda_idx=0, eval=False,               #
                     save_model='last', load_model_path=None):
@@ -27,48 +28,31 @@ def build_and_train(log_dir, task="TargetReach", environments=RLBench, run_ID=0,
     agent_state_dict = params.get('agent_state_dict')
     optimizer_state_dict = params.get('optimizer_state_dict')
 
-    action_repeat = 2
+    action_repeat = 2  # N
+    # affinity = affinity_from_code(slot_affinity_code)
+    # config = configs[config_key]
+    # variant = lload_variant(log_dir)
+    # config = update_config(config, variant)
 
     factory_method = make_wapper(
         base_class=environments,
         # wrapper_classes: list of wrapper classes in order inner-first, outer-last
         wrapper_classes=[ActionRepeat, NormalizeActions, TimeLimit],
         # list of kwargs dictionaries passed to the wrapper classes:
-        wrapper_kwargs=[dict(amount=action_repeat), dict(), dict(duration=250 / action_repeat)])
-        # you'll have: TimeLimit(NormalizeActions(ActionRepeat(RLBench,
-        #                        dict(amount=action_repeat),
-        #                                         dict(),
-        #                                                       dict(amount=action_repeat))
-        # so, how to pass arguments to base_class?
-    environments_args = {}
-    environments_eval_args = {}
-    if environments == DeepMindControl:
-        environments_args = {"name": task}
-        environments_eval_args = {"name": task}
-    if environments == RLBench:
-        # these arguments don't work, need to set the robot /them in rlbench_env
-        environments_args = {"config": {"robot": "sawyer"}}  # {task: task}}  # , "_env": ""}}
-        environments_eval_args = {"config": {"robot": "sawyer"}}  #"task": task}
-    else:
-        print(environments)
-#    if isinstance(environments, Atari):
-#        environments_args = dict(name=task)
-#        environments_eval_args = dict(name=task)
-    print(environments, RLBench, environments_args)
+        wrapper_kwargs=[dict(amount=action_repeat), dict(), dict(duration=1000 / action_repeat)])
 
-#    sampler = SerialSampler(
+    # set these in env_rlbench
+    # environments_args = {"config": {"robot": "sawyer"}}  # {task: task}}  # , "_env": ""}}
+    # environments_eval_args = {"config": {"robot": "sawyer"}}  #"task": task}
+
     sampler = CpuSampler(
-        EnvCls=factory_method,
+        EnvCls=factory_method,  # AtariEnv,  # (is simpler)
+        # env_kwargs=config["env"],
+        CollectorCls=CpuWaitResetCollector,
         TrajInfoCls=TrajInfo,
-        # env_kwargs allows passing the arguments to (JUST?) base_class: so base_class is a poor name choice,
-        # base_class should be named environment_class,
-        # unfortunately, SerialSampler is defined in RLPyt, we can replicate and overwrite it in this repo
-        # - to get rid of **kwargs
-        # - pass the env_kwargs to base_class in the factory method too
-        # - don't split SerialSampler in a super(BaseSampler) and inherited (SerialSampler) class
-        #     unless useful
-        env_kwargs=environments_args,
-        eval_env_kwargs=environments_eval_args,
+        env_kwargs=environments_args,  # config["env"]
+        eval_env_kwargs=environments_eval_args,  # N
+        # **config["sampler"],  # (probably contains the next 6 args)
         batch_T=1,  # batch_size?
         batch_B=1,  # batch_length?
         max_decorrelation_steps=0,
@@ -76,16 +60,17 @@ def build_and_train(log_dir, task="TargetReach", environments=RLBench, run_ID=0,
         eval_max_steps=int(10e3),
         eval_max_trajectories=5,
     )
-
     batch_size = 50
     batch_length = 50
-    algo = Dreamer(initial_optim_state_dict=optimizer_state_dict,
-                   batch_size=batch_size,
-                   batch_length=batch_length
+    algo = Dreamer(
+                   # optim_kwargs=config["optim"], **config["algo"] # (probably contain the next 3 args)
+                   initial_optim_state_dict=optimizer_state_dict,  # N
+                   batch_size=batch_size,  # N
+                   batch_length=batch_length  # N
                   )
-    # agent = DMCDreamerAgent(train_noise=0.3, eval_noise=0, expl_type="additive_gaussian",
-    #                         expl_min=None, expl_decay=None, initial_model_state_dict=agent_state_dict)
-    agent = BenchmarkDreamerAgent(train_noise=0.3,
+    agent = BenchmarkDreamerAgent(
+                                  # model_kwargs=config["model"], **config["agent"],  # # (probably contains the next 6 args)
+                                  train_noise=0.3,
                                   eval_noise=0,
                                   expl_type="additive_gaussian",
                                   expl_min=None, expl_decay=None,
@@ -95,11 +80,13 @@ def build_and_train(log_dir, task="TargetReach", environments=RLBench, run_ID=0,
         algo=algo,
         agent=agent,
         sampler=sampler,
-        n_steps=1e6,
-        log_interval_steps=1e3,
         # affinity=dict(cuda_idx=cuda_idx),
-        affinity=dict(workers_cpus=[0,1,2,3])
+        affinity=dict(workers_cpus=[1,2,3]),  # = affinity = affinity_from_code(slot_affinity_code)
+        **config["runner"],  # (probably contains the next 2 args)
+        n_steps=1e6,  # N
+        log_interval_steps=1e3,  # N
     )
+
     config = {"task": task}
     name = "dreamer_" + task
     with logger_context(log_dir, run_ID, name, config, snapshot_mode=save_model, override_prefix=True,
